57042cffed68b31d88a2297aa9309cc60675fa87
Игорь Максименко <mrdkigorm@gmail.com>
2022-01-26 17:07:30 +0200

    task 1

--- task1.ts
+++ task1.ts
@@ -1,75 +1,75 @@
+class ListNode <T> {
+    element: T;
+    index: number;
+    next: ListNode<T> | null  = null;
 
-let arr1 = ['a', 'b', 'c', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'];
-let arr2 = ['b', 'a', 'c', 'l', 'f', 'i', 'i'];
-
-function Node(element, index) {
-    this.element = element;
-    this.index = index;
-    this.next = null
+    constructor(element: T, index: number) {
+        this.element = element;
+        this.index = index;
+    }
 }
 
-function LinkedList() {
-    let length = 0
-    let head = null
-    this.size = function() {
-        return length
-    }
+class List <T> {
+    head: ListNode<T> | null = null;
+    constructor() {};
 
-    this.head = function() {
-        return head
-    }
-
-	//each node has 3 fields: next node, value and index, we just found place to insert next element and forn array from list element values
-    this.add = function(element, index) {
-        let node = new Node(element, index)
-        if (head === null) {
-            head = node
+    add(element:T, index: number): void {
+        let node = new ListNode <T> (element, index)
+        if (this.head === null) {
+            this.head = node
         } else {
-            let currentNode = head;
+            let currentNode = this.head;
             let prevNode = null;
            
             while (currentNode.index < index && currentNode.next) {
-            	prevNode = currentNode;
+                prevNode = currentNode;
                 currentNode = currentNode.next
             }
 
             if (!prevNode && currentNode.index >= index) {
-            	head = node;
-            	head.next = currentNode;
+                this.head = node;
+                this.head.next = currentNode;
             } else if (prevNode && currentNode.index >= index) {
-            	prevNode.next = node;
-            	node.next = currentNode;
+                prevNode.next = node;
+                node.next = currentNode;
             } else {
-            	currentNode.next = node;
+                currentNode.next = node;
             }
             
         }
-        length++
-    }
+    };
 
-    this.createArray = function() {
-		let resultArr = [];
-        let currentNode = head;
-        while (currentNode.next) {
-        	resultArr.push(currentNode.element);
-            currentNode = currentNode.next
+    toArray(): Array<T> {
+        let resultArr = [];
+        let currentNode = this.head;
+        if (currentNode) {
+            while (currentNode.next) {
+                resultArr.push(currentNode.element);
+                currentNode = currentNode.next
+            }
+            resultArr.push(currentNode.element);
         }
-        resultArr.push(currentNode.element);
-         return resultArr;  
-    }
+        
+        return resultArr;  
+    };
 }
 
-var list = new LinkedList();
+
+
+const arr1 = ['a', 'b', 'c', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l'];
+const arr2 = ['b', 'a', 'c', 'l', 'f', 'i', 'i', 'e'];
+
+let list = new List<string>();
 
 //we should go through shorter array because in case of too huge former array we'll have big time loss
 for (let elem of arr2) {
-	let index = 0;
-	while (elem !== arr1[index]) {
-		index++;
-	}
-	//to solve the issue we use list 
+ let index = 0;
+ while (elem !== arr1[index]) {
+     index++;
+ }
+ //to solve the issue we use list 
 
-	list.add(elem, index);
+ list.add(elem, index);
 }
 
-console.log(list.createArray());
+console.log(list.toArray());
